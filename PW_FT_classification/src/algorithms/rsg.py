8b5509a6f97a1f8c72013a3fc693c510b9f9bc4a
Joejyn Wan <joejyn@gmail.com>
2024-09-03 13:49:21 +0800

    Implement RSG Resnet model

---
+++ PW_FT_classification/src/algorithms/rsg.py
@@ -0,0 +1,304 @@
+import os
+import warnings
+import numpy as np
+from datetime import datetime
+from tqdm import tqdm
+import random
+import pandas as pd
+
+import torch
+import torch.optim as optim
+import pytorch_lightning as pl
+
+from .utils import acc
+from ...src import models
+
+
+__all__ = [
+    'RSG'
+]
+
+class RSG(pl.LightningModule):
+    """
+    Defines the architecture for training a model using PyTorch Lightning.
+
+    This class inherits from PyTorch Lightning's LightningModule and sets up the model, optimizers,
+    and training/validation/testing steps for the training process.
+    """
+
+    name = 'RSG'
+
+    def __init__(self, conf, train_class_counts, id_to_labels, head_lists, epoch_thresh, **kwargs):
+        """
+        Initializes the RSG model.
+
+        Args:
+            conf: Configuration object with model parameters.
+            train_class_counts: Counts of training classes.
+            id_to_labels: Mapping from IDs to label names.
+            **kwargs: Additional keyword arguments.
+        """
+        super().__init__()
+        self.hparams.update(conf.__dict__)
+        self.save_hyperparameters(ignore=['conf', 'train_class_counts'])
+        self.train_class_counts = train_class_counts
+        self.id_to_labels = id_to_labels
+        self.net = models.__dict__[self.hparams.model_name](train_class_counts=train_class_counts,
+                                                            num_cls=self.hparams.num_classes, 
+                                                            num_layers=self.hparams.num_layers,
+                                                            loss_type=self.hparams.loss_type,
+                                                            head_lists=head_lists, 
+                                                            phase_train=True, 
+                                                            epoch_thresh=epoch_thresh)
+
+    def configure_optimizers(self):
+        """
+        Configures the optimizers and learning rate schedulers.
+
+        Returns:
+            Tuple[List, List]: A tuple containing the list of optimizers and the list of learning rate schedulers.
+        """
+        # Define parameters for the optimizer
+        net_optim_params_list = [
+            # Optimizer parameters for feature extraction
+            {'params': self.net.feature.parameters(),
+             'lr': self.hparams.lr_feature,
+             'momentum': self.hparams.momentum_feature,
+             'weight_decay': self.hparams.weight_decay_feature},
+            # Optimizer parameters for the classifier
+            {'params': self.net.classifier.parameters(),
+             'lr': self.hparams.lr_classifier,
+             'momentum': self.hparams.momentum_classifier,
+             'weight_decay': self.hparams.weight_decay_classifier}
+        ]
+        # Setup optimizer and optimizer scheduler
+        optimizer = torch.optim.SGD(net_optim_params_list)
+        scheduler = optim.lr_scheduler.StepLR(optimizer, 
+                                              step_size=self.hparams.step_size, 
+                                              gamma=self.hparams.gamma)   
+        return [optimizer], [scheduler]
+
+    def on_train_start(self):
+        """
+        Hook function called at the start of training. Initializes best accuracy and the network.
+        """
+        self.best_acc = 0
+        self.net.feat_init()
+        self.net.setup_criteria()
+
+    def on_train_epoch_start(self):
+        """
+        Hook function called at the start of each epoch during training. 
+        Initializes training procedure, adapted from https://github.com/kaidic/LDAM-DRW.git.
+        """
+        if self.hparams.train_rule == 'None':
+            self.per_cls_weights = None 
+        elif self.hparams.train_rule == 'Reweight':
+            beta = 0.9999
+            effective_num = 1.0 - np.power(beta, self.train_class_counts)
+            per_cls_weights = (1.0 - beta) / np.array(effective_num)
+            per_cls_weights = per_cls_weights / np.sum(per_cls_weights)*len(self.train_class_counts)
+            self.per_cls_weights = torch.tensor(per_cls_weights, dtype=torch.float32, device='cuda')
+        elif self.hparams.train_rule == 'DRW':
+            idx = self.current_epoch // 75 #Start re-weighting after epoch 75
+            betas = [0, 0.9999]
+            effective_num = 1.0 - np.power(betas[idx], self.train_class_counts)
+            per_cls_weights = (1.0 - betas[idx]) / np.array(effective_num)
+            per_cls_weights = per_cls_weights / np.sum(per_cls_weights)*len(self.train_class_counts)
+            self.per_cls_weights = torch.tensor(per_cls_weights, dtype=torch.float32, device='cuda')
+        else:
+            warnings.warn('Sample rule is not listed')
+
+    def training_step(self, batch, batch_idx):
+        """
+        Training step for each batch.
+
+        Args:
+            batch: The current batch of data.
+            batch_idx: The index of the current batch.
+
+        Returns:
+            Tensor: The loss for the current training step.
+        """
+        data, label_ids = batch[0], batch[1]
+        
+        # Forward pass
+        feats, cesc_loss, total_mv_loss, combine_target = self.net.feature(
+            data, epoch=self.current_epoch, batch_target=label_ids, phase_train=True)
+        logits = self.net.classifier(feats)
+
+        # Calculate loss
+        loss = self.net.criterion_cls(logits, combine_target, weight = self.per_cls_weights) 
+        loss = loss + 0.1 * cesc_loss.mean() + 0.01 * total_mv_loss.mean()
+        self.log("train_loss", loss)
+        
+        return loss
+
+    def on_validation_start(self):
+        """
+        Hook function called at the start of validation. Initializes storage for validation outputs.
+        """
+        self.val_st_outs = []
+
+    def validation_step(self, batch, batch_idx):
+        """
+        Validation step for each batch.
+
+        Args:
+            batch: The current batch of data.
+            batch_idx: The index of the current batch.
+        """
+        data, label_ids = batch[0], batch[1]
+
+        # Forward pass
+        feats = self.net.feature(data, phase_train=False)
+        logits = self.net.classifier(feats)
+        preds = logits.argmax(dim=1)
+        
+        self.val_st_outs.append((preds.detach().cpu().numpy(),
+                                 label_ids.detach().cpu().numpy()))
+
+    def on_validation_epoch_end(self):
+        """
+        Hook function called at the end of the validation epoch. Aggregates and logs validation results.
+        """
+        total_preds = np.concatenate([x[0] for x in self.val_st_outs], axis=0)
+        total_label_ids = np.concatenate([x[1] for x in self.val_st_outs], axis=0)
+        self.eval_logging(total_preds, total_label_ids)
+
+    def on_test_start(self):
+        """
+        Hook function called at the start of testing. Initializes storage for test outputs.
+        """
+        self.te_st_outs = []
+
+    def test_step(self, batch, batch_idx):
+        """
+        Test step for each batch.
+
+        Args:
+            batch: The current batch of data, including metadata.
+            batch_idx: The index of the current batch.
+        """
+        data, label_ids, labels, file_ids = batch
+        # Forward pass
+        feats = self.net.feature(data, phase_train=False)
+        logits = self.net.classifier(feats)
+        preds = logits.argmax(dim=1)
+        
+        self.te_st_outs.append((preds.detach().cpu().numpy(),
+                               label_ids.detach().cpu().numpy(),
+                               feats.detach().cpu().numpy(),
+                               logits.detach().cpu().numpy(), 
+                               labels, file_ids 
+                               ))
+    
+
+    def on_test_epoch_end(self):
+        """
+        Hook function called at the end of the test epoch. Aggregates and logs test results, and saves output.
+        """
+        # Concatenate outputs from all test steps
+        total_preds = np.concatenate([x[0] for x in self.te_st_outs], axis=0)
+        total_label_ids = np.concatenate([x[1] for x in self.te_st_outs], axis=0)
+        total_feats = np.concatenate([x[2] for x in self.te_st_outs], axis=0)
+        total_logits = np.concatenate([x[3] for x in self.te_st_outs], axis=0)
+        total_labels = np.concatenate([x[4] for x in self.te_st_outs], axis=0)
+        total_file_ids = np.concatenate([x[5] for x in self.te_st_outs], axis=0)
+
+        # Calculate the metrics and save the output
+        class_acc_df = self.eval_logging(total_preds[total_label_ids != -1],
+                                         total_label_ids[total_label_ids != -1],
+                                         print_class_acc = False)
+        class_acc_csv_path = os.path.join(self.hparams.save_dir, 'class_acc.csv') 
+        class_acc_df.to_csv(class_acc_csv_path, index = False)
+
+        eval_csv = pd.DataFrame(data={"file_ids": total_file_ids,
+                                      "preds": total_preds,
+                                      "label_ids": total_label_ids,
+                                      "labels": total_labels})
+        eval_csv_path = os.path.join(self.hparams.save_dir, 'test_eval.csv') 
+        eval_csv.to_csv(eval_csv_path, index = False)
+
+        npz_path = os.path.join(self.hparams.save_dir, 'test_eval.npz') 
+        np.savez(npz_path, preds=total_preds, label_ids=total_label_ids, feats=total_feats,
+                 logits=total_logits, labels=total_labels, file_ids=total_file_ids)  
+        print('Test outputs saved to {}.'.format(self.hparams.save_dir))
+
+    def on_predict_start(self):
+        """
+        Hook function called at the start of prediction. Initializes storage for prediction outputs.
+        """
+        self.pr_st_outs = []
+
+    def predict_step(self, batch, batch_idx):
+        """
+        Prediction step for each batch.
+
+        Args:
+            batch: The current batch of data, including metadata.
+            batch_idx: The index of the current batch.
+        """
+        data, file_ids = batch
+        # Forward pass
+        feats = self.net.feature(data, phase_train=False)
+        logits = self.net.classifier(feats)
+        preds = logits.argmax(dim=1)
+        
+        self.pr_st_outs.append((preds.detach().cpu().numpy(),
+                               feats.detach().cpu().numpy(),
+                               logits.detach().cpu().numpy(), 
+                               file_ids 
+                               ))
+    
+
+    def on_predict_epoch_end(self):
+        """
+        Hook function called at the end of the predict epoch. Aggregates and saves prediction outputs.
+        """
+        # Concatenate outputs from all predict steps
+        total_preds = np.concatenate([x[0] for x in self.pr_st_outs], axis=0)
+        total_feats = np.concatenate([x[1] for x in self.pr_st_outs], axis=0)
+        total_logits = np.concatenate([x[2] for x in self.pr_st_outs], axis=0)
+        total_file_ids = np.concatenate([x[3] for x in self.pr_st_outs], axis=0)
+
+        output_path = self.hparams.evaluate.replace('.ckpt', '_predict.npz') 
+        np.savez(output_path, preds=total_preds, feats=total_feats,
+                 logits=total_logits, file_ids=total_file_ids)  
+        print('Predict output saved to {}.'.format(output_path))
+
+
+    def eval_logging(self, preds, labels, print_class_acc=False):
+        """
+        Logs evaluation metrics such as accuracy.
+
+        Args:
+            preds: Predictions from the model.
+            labels: Ground truth labels.
+            print_class_acc (bool): Flag to print class-wise accuracy.
+        """
+        class_acc, mac_acc, mic_acc = acc(preds, labels)
+
+        self.log("valid_mac_acc", mac_acc * 100)
+        self.log("valid_mic_acc", mic_acc * 100)
+
+        unique_eval_labels = np.unique(labels)
+        acc_list = [(class_acc[i], unique_eval_labels[i],
+                     self.id_to_labels[unique_eval_labels[i]],
+                     self.train_class_counts[unique_eval_labels[i]])
+                     for i in range(len(class_acc))]
+        
+        if print_class_acc:
+            print('\n')
+            for i in range(len(class_acc)):
+                info = '{:>20} ({:<3}, tr {:>3}) Acc: '.format(acc_list[i][2],
+                                                               acc_list[i][1],
+                                                               acc_list[i][3])
+                info += '{:.2f}'.format(acc_list[i][0] * 100)
+                print(info)
+
+        acc_df = pd.DataFrame(acc_list,columns=['acc', 'label_ids', 'labels', 'train_class_counts'])
+        acc_df = acc_df[['label_ids', 'labels', 'train_class_counts', 'acc']]
+        acc_df["acc"] = pd.to_numeric(acc_df["acc"])
+
+        return(acc_df)
---
+++ PW_FT_classification/src/models/rsg_resnet.py
@@ -0,0 +1,308 @@
+import os
+import copy
+import warnings
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+from torchvision.models.resnet import BasicBlock, Bottleneck, conv1x1
+from .losses import LDAMLoss, FocalLoss
+from .RSG import *
+
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_state_dict_from_url
+
+# Exportable class names for external use
+__all__ = [
+    'RSGResNet'
+]
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-f37072fd.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-0676ba61.pth'
+}
+
+
+class NormedLinear(nn.Module):
+
+    def __init__(self, in_features, out_features):
+        super(NormedLinear, self).__init__()
+        self.weight = Parameter(torch.Tensor(in_features, out_features))
+        self.weight.data.uniform_(-1, 1).renorm_(2, 1, 1e-5).mul_(1e5)
+
+    def forward(self, x):
+        out = (F.normalize(x, dim=1)).mm(F.normalize(self.weight, dim=0))
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, head_lists=[], zero_init_residual=False,
+                 groups=1, width_per_group=64, phase_train=True, epoch_thresh=0, 
+                 replace_stride_with_dilation=None, norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        self.phase_train = phase_train
+        
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        if self.phase_train:
+           self.head_lists = head_lists
+           self.RSG = RSG(n_center = 15, feature_maps_shape = [256*block.expansion, 14, 14], 
+                          num_classes = num_classes, contrastive_module_dim = 256, 
+                          head_class_lists = self.head_lists, epoch_thresh = epoch_thresh)
+
+        # self.fc_ = NormedLinear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+    
+    def _forward_impl(self, x, epoch=0, batch_target=None, phase_train=False):
+        """
+        Forward pass implementation for the ResNet backbone.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after applying the ResNet backbone.
+        """
+        # Applying the ResNet layers and operations
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        if phase_train:
+            x, cesc_total, loss_mv_total, combine_target = self.RSG.forward(
+                x, self.head_lists, batch_target, epoch
+            )
+
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        if phase_train:
+            return x, cesc_total, loss_mv_total, combine_target
+        else:
+            return x
+        
+    def forward(self, x, epoch=0, batch_target=None, phase_train=True):
+        return self._forward_impl(x, epoch, batch_target, phase_train)
+
+
+class ResNetBackbone(ResNet):
+    """
+    Custom ResNet backbone class for feature extraction.
+
+    Inherits from the torchvision ResNet class and allows customization of the architecture.
+    """
+
+    def __init__(
+        self,
+        block,
+        layers,
+        num_classes,
+        head_lists=[],
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        phase_train=False,
+        epoch_thresh=0,
+        replace_stride_with_dilation=None,
+        norm_layer=None,
+    ):
+        """
+        Initialize the ResNet backbone.
+
+        Args:
+            block (nn.Module): Type of block to use (BasicBlock or Bottleneck).
+            layers (list of int): Number of layers in each block.
+            zero_init_residual (bool): Zero-initialize the last BN in each residual branch.
+            groups (int): Number of groups for group normalization.
+            width_per_group (int): Width per group.
+            replace_stride_with_dilation (list of bool or None): Use dilation instead of stride.
+            norm_layer (callable or None): Norm layer to use.
+        """
+        super(ResNetBackbone, self).__init__(
+            block=block,
+            layers=layers,
+            num_classes=num_classes,
+            head_lists=head_lists,
+            zero_init_residual=zero_init_residual,
+            groups=groups,
+            width_per_group=width_per_group,
+            phase_train=phase_train,
+            epoch_thresh=epoch_thresh,
+            replace_stride_with_dilation=replace_stride_with_dilation,
+            norm_layer=norm_layer,
+        )
+
+
+class RSGResNet(nn.Module):
+    """
+    Custom ResNet classifier class.
+
+    Extends nn.Module and provides a complete ResNet-based classifier, including feature extraction and classification layers.
+    """
+
+    name = 'RSGResNet'
+
+    def __init__(self, train_class_counts, num_cls=10, num_layers=18, loss_type="CE", 
+                 head_lists=[], phase_train=False, epoch_thresh=0):
+        """
+        Initialize the RSGResNet.
+
+        Args:
+            num_cls (int): Number of classes for the classifier.
+            num_layers (int): Number of layers in the ResNet model (e.g., 18, 50).
+        """
+        super(RSGResNet, self).__init__()
+        self.num_cls = num_cls
+        self.num_layers = num_layers
+        self.loss_type = loss_type
+        self.train_class_counts = train_class_counts
+        self.feature = None
+        self.classifier = None
+        self.criterion_cls = None
+        self.head_lists = head_lists
+        self.phase_train = phase_train
+        self.epoch_thresh = epoch_thresh
+
+        # Initialize the network with the specified settings
+        self.setup_net()
+
+    def setup_net(self):
+        """
+        Set up the ResNet network and initialize its weights.
+        """
+        kwargs = {}
+
+        # Selecting the appropriate ResNet architecture and pre-trained weights
+        if self.num_layers == 18:
+            block = BasicBlock
+            layers = [2, 2, 2, 2]
+            #self.pretrained_weights = ResNet18_Weights.IMAGENET1K_V1
+            self.pretrained_weights = state_dict = load_state_dict_from_url(model_urls['resnet18'],
+                                              progress=True)
+        elif self.num_layers == 50:
+            block = Bottleneck
+            layers = [3, 4, 6, 3]
+            #self.pretrained_weights = ResNet50_Weights.IMAGENET1K_V1
+            self.pretrained_weights = state_dict = load_state_dict_from_url(model_urls['resnet50'],
+                                              progress=True)
+        else:
+            raise Exception('ResNet Type not supported.')
+
+        # Constructing the feature extractor and classifier
+        self.feature = ResNetBackbone(block, layers, num_classes=self.num_cls, 
+                                      head_lists=self.head_lists, phase_train=self.phase_train, 
+                                      epoch_thresh=self.epoch_thresh, **kwargs)
+        self.classifier = NormedLinear(512 * block.expansion, self.num_cls)
+
+    def setup_criteria(self):
+        """
+        Set up the criterion for the classifier.
+        """
+        if self.loss_type == "CE":
+            self.criterion_cls = nn.CrossEntropyLoss()
+        elif self.loss_type == "LDAM":
+            self.criterion_cls = LDAMLoss(cls_num_list=self.train_class_counts, max_m=0.5, s=30)
+        elif self.loss_type == 'Focal':
+            self.criterion_cls = FocalLoss(gamma=1)
+        else:
+            warnings.warn('Loss type is not listed')
+            return
+
+    def feat_init(self):
+        """
+        Initialize the feature extractor with pre-trained weights.
+        """
+        # Load pre-trained weights and adjust for the current model
+        #init_weights = self.pretrained_weights.get_state_dict(progress=True)
+        init_weights = self.pretrained_weights
+        init_weights = OrderedDict({k.replace('module.', '').replace('feature.', ''): init_weights[k]
+                                    for k in init_weights})
+
+        # Load the weights into the feature extractor
+        self.feature.load_state_dict(init_weights, strict=False)
+
+        # Identify missing and unused keys in the loaded weights
+        load_keys = set(init_weights.keys())
+        self_keys = set(self.feature.state_dict().keys())
+
+        missing_keys = self_keys - load_keys
+        unused_keys = load_keys - self_keys
+        print('missing keys: {}'.format(sorted(list(missing_keys))))
+        print('unused_keys: {}'.format(sorted(list(unused_keys))))
