c8a77623f96f97a4ae7867c0a1699ac9c60eaaca
Joejyn Wan <joejyn@gmail.com>
2025-04-15 11:13:19 +0800

    Add verbose option in yolov8 for video processing

--- PytorchWildlife/models/detection/ultralytics_based/yolov8_base.py
+++ PytorchWildlife/models/detection/ultralytics_based/yolov8_base.py
@@ -118,7 +118,8 @@ class YOLOV8Base(BaseDetector):
         return results
         
 
-    def single_image_detection(self, img, img_path=None, det_conf_thres=0.2, id_strip=None):
+    def single_image_detection(self, img, img_path=None, det_conf_thres=0.2, id_strip=None, 
+                               verbose=True):
         """
         Perform detection on a single image.
         
@@ -144,6 +145,7 @@ class YOLOV8Base(BaseDetector):
 
         self.predictor.args.batch = 1
         self.predictor.args.conf = det_conf_thres
+        self.predictor.args.verbose = verbose
         det_results = list(self.predictor.stream_inference([img]))
         
         return self.results_generation(det_results[0], img_path, id_strip)
